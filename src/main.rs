d6074bc4f29ef92aeaf56129a0f66937f8a7f733
jhossepmartinez <jhossepmv@gmail.com>
2026-06-09 17:20:50 -0400

    chore: force the action

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     println!("Listening at port: 3000!");
     axum::serve(listener, app).await.unwrap();
 }
